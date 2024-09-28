fb19001a38e72a633e34227814a155cdc4c458e7
gama <gamaroot@gmail.com>
2024-09-28 18:16:50 -0400

    feat: considering Unity internal tags in the TagEnumGenerator.cs

--- Assets/Project/Modules/Utils/Scripts/Editor/TagEnumGenerator.cs
+++ Assets/Project/Modules/Utils/Scripts/Editor/TagEnumGenerator.cs
@@ -8,6 +8,17 @@ namespace Utils
     {
         private const string PATH = "Assets/Project/Modules/Utils/Scripts/Tags.cs";
 
+        private static readonly string[] INTERNAL_TAGS = new string[]
+        {
+            "Untagged",
+            "Respawn",
+            "Finish",
+            "EditorOnly",
+            "MainCamera",
+            "Player",
+            "GameController",
+        };
+
         // Menu item to manually regenerate the enum file
         [MenuItem("Tools/Update Tags Enum")]
         public static void GenerateTagEnum()
@@ -18,9 +29,17 @@ namespace Utils
 
             // Build the enum content
             string enumContent = "namespace Utils\n{\n\tpublic enum Tags \n\t{\n";
-            for (int i = 0; i < tagsProp.arraySize; i++)
+
+            // Adding Unity internal tags
+            for (int index = 0; index < INTERNAL_TAGS.Length; index++)
             {
-                SerializedProperty t = tagsProp.GetArrayElementAtIndex(i);
+                string tag = INTERNAL_TAGS[index];
+                enumContent += "\t\t" + tag + ",\n";
+            }
+
+            for (int index = 0; index < tagsProp.arraySize; index++)
+            {
+                SerializedProperty t = tagsProp.GetArrayElementAtIndex(index);
                 string tag = t.stringValue;
 
                 // Create a valid enum entry (sanitize to avoid spaces and invalid characters)
--- Assets/Project/Modules/Utils/Scripts/Tags.cs
+++ Assets/Project/Modules/Utils/Scripts/Tags.cs
@@ -2,6 +2,13 @@ namespace Utils
 {
 	public enum Tags 
 	{
+		Untagged,
+		Respawn,
+		Finish,
+		EditorOnly,
+		MainCamera,
+		Player,
+		GameController,
 		PlayerCamera,
 		Missile,
 		Target,
