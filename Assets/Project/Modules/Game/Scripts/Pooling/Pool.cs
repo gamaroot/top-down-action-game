0a1db847be0abff05f32e31d5adb67a20d9c1042
gama <gamaroot@gmail.com>
2024-09-29 16:39:16 -0400

    feat: creating a pooling system

---
+++ Assets/Project/Modules/Game/Scripts/Pooling/Pool.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game
+{
+    [Serializable]
+    public class Pool
+    {
+        public int ExpandedPoolSize = 1;
+
+        private readonly GameObject _resourcePrefab;
+        private readonly Stack<GameObject> _pool = new();
+        private readonly Transform _parentGameObject;
+        private readonly Action<GameObject> _onObjectCreated;
+        private readonly Action<GameObject> _onObjectActivated;
+
+        public Pool(Transform parentGameObject, GameObject resourcePrefab,
+                    Action<GameObject> onObjectCreated = null,
+                    Action<GameObject> onObjectActivated = null)
+        {
+            var node = new GameObject(resourcePrefab.name);
+            node.transform.SetParent(parentGameObject);
+
+            this._parentGameObject = node.transform;
+            this._resourcePrefab = resourcePrefab;
+            this._onObjectCreated = onObjectCreated;
+            this._onObjectActivated = onObjectActivated;
+
+            this.AddObjectsToPool(this.ExpandedPoolSize);
+        }
+
+        public T BorrowObject<T>(float autoDisableInSeconds = -1f)
+        {
+            this.EnsurePoolHasObjects();
+
+            GameObject obj = this._pool.Pop();
+            this._onObjectActivated?.Invoke(obj);
+
+            PoolingObject poolingObj = obj.GetComponent<PoolingObject>();
+
+            if (typeof(T) == typeof(AudioSource))
+            {
+                AudioSource audioSource = obj.GetComponent<AudioSource>();
+                poolingObj.SetAutoDisable(Mathf.Max(1f, audioSource.clip.length));
+            }
+            else
+            {
+                poolingObj.SetAutoDisable(autoDisableInSeconds);
+            }
+
+            return obj.GetComponent<T>();
+        }
+
+        public void ReturnToPool(GameObject obj)
+        {
+            obj.transform.SetParent(this._parentGameObject);
+            this._pool.Push(obj);
+        }
+
+        public Transform GetParent()
+        {
+            return this._parentGameObject;
+        }
+
+        public void DisableAll()
+        {
+            if (!this._parentGameObject)
+                return;
+
+            foreach (PoolingObject item in this._parentGameObject.GetComponentsInChildren<PoolingObject>())
+            {
+                item.Disable();
+            }
+        }
+
+        private void AddObjectsToPool(int quantity)
+        {
+            for (int index = 0; index < quantity; index++)
+            {
+                var newObj = GameObject.Instantiate(this._resourcePrefab);
+                newObj.name += index;
+                newObj.transform.SetParent(this._parentGameObject);
+                newObj.SetActive(false);
+
+                this._onObjectCreated?.Invoke(newObj);
+
+                PoolingObject poolingObj = newObj.AddComponent<PoolingObject>();
+                poolingObj.Initialize(this);
+
+                this._pool.Push(newObj);
+            }
+        }
+
+        private void EnsurePoolHasObjects()
+        {
+            if (this._pool.Count == 0)
+                this.AddObjectsToPool(this.ExpandedPoolSize);
+        }
+    }
+}
---
+++ Assets/Project/Modules/Game/Scripts/Pooling/PoolingObject.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+namespace Game
+{
+    public class PoolingObject : MonoBehaviour
+    {
+        private Pool _pool;
+        private float _autoDisableTimer = -1f;
+
+        private void OnEnable()
+        {
+            base.transform.SetParent(this._pool.GetParent());
+
+            if (this._autoDisableTimer > 0)
+                base.Invoke(nameof(Disable), this._autoDisableTimer);
+        }
+
+        private void OnDisable()
+        {
+            this._pool?.ReturnToPool(base.gameObject);
+        }
+
+        public void Initialize(Pool resourcePool)
+        {
+            this._pool = resourcePool;
+        }
+
+        public void Disable()
+        {
+            base.gameObject.SetActive(false);
+        }
+
+        public void SetAutoDisable(float time)
+        {
+            this._autoDisableTimer = time;
+        }
+    }
+}
