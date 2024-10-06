9de15021cea0a6c0826be3a32d1b435173198688
gama <gamaroot@gmail.com>
2024-10-06 11:23:03 -0400

    feat: implementing dash

---
+++ Assets/Project/Modules/Game/Scripts/Character/Player/DashHandler.cs
@@ -0,0 +1,69 @@
+using UnityEngine;
+
+public class DashHandler
+{
+    public bool CanDash { get; private set; } = true;
+    public bool IsDashing { get; private set; }
+    public Vector3 DashMovement { get; private set; }
+
+    private readonly float _dashSpeed;
+    private readonly float _dashDuration;
+    private readonly float _dashCooldown;
+
+    private float _dashStartTime;     // Track the start time of the dash
+    private Vector3 _dashDirection;   // Dash direction
+    private float _dashCooldownTimer; // Timer for dash cooldown
+
+    public DashHandler(float dashSpeed, float dashDuration, float dashCooldown)
+    {
+        this._dashSpeed = dashSpeed;
+        this._dashDuration = dashDuration;
+        this._dashCooldown = dashCooldown;
+    }
+
+    public void Dash(Vector2 dashInput)
+    {
+        if (!this.CanDash && dashInput == Vector2.zero)
+            return;
+
+        this.CanDash = false;
+        this.IsDashing = true;
+        Debug.Log(this.IsDashing);
+        this._dashStartTime = Time.time;  // Set the start time of the dash
+        this._dashDirection = new Vector3(dashInput.x, 0, dashInput.y).normalized;
+
+        // Start the cooldown timer
+        this._dashCooldownTimer = this._dashCooldown;
+    }
+
+    public void OnUpdate()
+    {
+
+        if (this.IsDashing)
+        {
+            float dashElapsedTime = Time.time - this._dashStartTime;
+            if (dashElapsedTime < this._dashDuration)
+            {
+                // Continue dashing in the stored direction
+                this.DashMovement = this._dashDirection * this._dashSpeed * Time.deltaTime;
+            }
+            else
+            {
+                // Dash ended
+                this.IsDashing = false;
+            }
+            return;
+        }
+
+        // Handle dash cooldown
+        if (this._dashCooldownTimer <= 0f)
+        {
+            this.CanDash = true;
+            this._dashCooldownTimer = 0;
+        }
+        else
+        {
+            this._dashCooldownTimer -= Time.deltaTime;
+        }
+    }
+}
--- Assets/Project/Modules/Game/Scripts/Character/Player/PlayerMovementController.cs
+++ Assets/Project/Modules/Game/Scripts/Character/Player/PlayerMovementController.cs
@@ -1,6 +1,6 @@
 using System;
+using System.Collections;
 using UnityEngine;
-using UnityEngine.AI;
 using UnityEngine.InputSystem;
 using Utils;
 
@@ -11,13 +11,20 @@ namespace Game
     {
         [Header("Attributes")]
         [SerializeField] private float _speed = 10f;
+        [SerializeField] private float _dashSpeed = 30f;      // Speed multiplier during dash
+        [SerializeField] private float _dashDuration = 0.2f;  // Duration of the dash
+        [SerializeField] private float _dashCooldown = 1f;    // Cooldown time between dashes
         [SerializeField] private LayerMask _groundLayerMask;
 
         [Header("Components")]
         [SerializeField] private CharacterController _controller;
 
+        private bool IsGamepad => Gamepad.current != null;
+        private Vector3 NormalMovement => this._speed * Time.deltaTime * new Vector3(this._move.x, 0, this._move.y);
+
         private Vector2 _move;
         private InputController _inputs;
+        private DashHandler _dashHandler;
 
         private void OnValidate()
         {
@@ -27,9 +34,12 @@ namespace Game
 
         private void Awake()
         {
+            this._dashHandler = new(this._dashSpeed, this._dashDuration, this._dashCooldown);
+
             this._inputs = new InputController();
             this._inputs.Player.Move.performed += context => this._move = context.ReadValue<Vector2>();
             this._inputs.Player.Move.canceled += context => this._move = Vector2.zero;
+            this._inputs.Player.Dash.performed += context => this.OnDashPressed();
         }
 
         private void OnEnable()
@@ -44,13 +54,14 @@ namespace Game
 
         private void Update()
         {
-            Vector3 movement = this._speed * Time.deltaTime * new Vector3(this._move.x, 0, this._move.y);
-            this.Move(movement);
-
-            if (Gamepad.current != null)
+            if (this.IsGamepad)
                 this.RotateToGamepadDirection();
             else
                 this.RotateToMousePoint();
+
+            this._dashHandler.OnUpdate();
+
+            this.Move(this._dashHandler.IsDashing ? this._dashHandler.DashMovement : this.NormalMovement);
         }
 
         public override void Move(Vector3 point)
@@ -71,8 +82,7 @@ namespace Game
             {
                 // Get the direction from the player to the hit point
                 Vector3 direction = hit.point - this._controller.transform.position;
-
-                this.RotateToDirection(new Vector3(direction.x, direction.z));
+                this.RotateToDirection(new Vector2(direction.x, direction.z));
             }
         }
 
@@ -90,6 +100,15 @@ namespace Game
             // Ensure the direction vector is normalized and ignore the y-axis
             var flatDirection = new Vector3(direction.x, 0, direction.y);
             this._controller.transform.rotation = Quaternion.LookRotation(flatDirection);
+        }
+
+        private void OnDashPressed()
+        {
+            if (!this._dashHandler.CanDash)
+                return;
+
+            Vector2 dashDirection = this.IsGamepad ? Gamepad.current.leftStick.value : this._move;
+            this._dashHandler.Dash(dashDirection);
         }
     }
 }
