3aa615316cce958ce846a7926eab9c534c0ef3ae
gama <gamaroot@gmail.com>
2024-10-06 12:21:18 -0400

    refactor: defining DashHandler.cs namespace as "Game"

--- Assets/Project/Modules/Game/Scripts/Character/DashHandler.cs
+++ Assets/Project/Modules/Game/Scripts/Character/DashHandler.cs
@@ -1,69 +1,72 @@
 using UnityEngine;
 
-public class DashHandler
+namespace Game
 {
-    public bool CanDash { get; private set; } = true;
-    public bool IsDashing { get; private set; }
-    public Vector3 DashMovement { get; private set; }
-
-    private readonly float _dashSpeed;
-    private readonly float _dashDuration;
-    private readonly float _dashCooldown;
-
-    private float _dashStartTime;     // Track the start time of the dash
-    private Vector3 _dashDirection;   // Dash direction
-    private float _dashCooldownTimer; // Timer for dash cooldown
-
-    public DashHandler(float dashSpeed, float dashDuration, float dashCooldown)
+    public class DashHandler
     {
-        this._dashSpeed = dashSpeed;
-        this._dashDuration = dashDuration;
-        this._dashCooldown = dashCooldown;
-    }
+        public bool CanDash { get; private set; } = true;
+        public bool IsDashing { get; private set; }
+        public Vector3 DashMovement { get; private set; }
 
-    public void Dash(Vector2 dashInput)
-    {
-        if (!this.CanDash && dashInput == Vector2.zero)
-            return;
+        private readonly float _dashSpeed;
+        private readonly float _dashDuration;
+        private readonly float _dashCooldown;
 
-        this.CanDash = false;
-        this.IsDashing = true;
+        private float _dashStartTime;     // Track the start time of the dash
+        private Vector3 _dashDirection;   // Dash direction
+        private float _dashCooldownTimer; // Timer for dash cooldown
 
-        this._dashStartTime = Time.time;  // Set the start time of the dash
-        this._dashDirection = new Vector3(dashInput.x, 0, dashInput.y).normalized;
-
-        // Start the cooldown timer
-        this._dashCooldownTimer = this._dashCooldown;
-    }
-
-    public void OnUpdate()
-    {
-
-        if (this.IsDashing)
+        public DashHandler(float dashSpeed, float dashDuration, float dashCooldown)
         {
-            float dashElapsedTime = Time.time - this._dashStartTime;
-            if (dashElapsedTime < this._dashDuration)
+            this._dashSpeed = dashSpeed;
+            this._dashDuration = dashDuration;
+            this._dashCooldown = dashCooldown;
+        }
+
+        public void Dash(Vector2 dashInput)
+        {
+            if (!this.CanDash && dashInput == Vector2.zero)
+                return;
+
+            this.CanDash = false;
+            this.IsDashing = true;
+
+            this._dashStartTime = Time.time;  // Set the start time of the dash
+            this._dashDirection = new Vector3(dashInput.x, 0, dashInput.y).normalized;
+
+            // Start the cooldown timer
+            this._dashCooldownTimer = this._dashCooldown;
+        }
+
+        public void OnUpdate()
+        {
+
+            if (this.IsDashing)
             {
-                // Continue dashing in the stored direction
-                this.DashMovement = this._dashDirection * this._dashSpeed * Time.deltaTime;
+                float dashElapsedTime = Time.time - this._dashStartTime;
+                if (dashElapsedTime < this._dashDuration)
+                {
+                    // Continue dashing in the stored direction
+                    this.DashMovement = this._dashDirection * this._dashSpeed * Time.deltaTime;
+                }
+                else
+                {
+                    // Dash ended
+                    this.IsDashing = false;
+                }
+                return;
+            }
+
+            // Handle dash cooldown
+            if (this._dashCooldownTimer <= 0f)
+            {
+                this.CanDash = true;
+                this._dashCooldownTimer = 0;
             }
             else
             {
-                // Dash ended
-                this.IsDashing = false;
+                this._dashCooldownTimer -= Time.deltaTime;
             }
-            return;
-        }
-
-        // Handle dash cooldown
-        if (this._dashCooldownTimer <= 0f)
-        {
-            this.CanDash = true;
-            this._dashCooldownTimer = 0;
-        }
-        else
-        {
-            this._dashCooldownTimer -= Time.deltaTime;
         }
     }
 }
