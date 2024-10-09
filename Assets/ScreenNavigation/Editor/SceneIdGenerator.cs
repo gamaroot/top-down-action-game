336aa743b8d706051bd0c5e2e594550f258e996f
gama <gamaroot@gmail.com>
2024-10-09 11:18:38 -0400

    feat: creating SceneID.cs enum file generator

---
+++ Assets/ScreenNavigation/Editor/SceneIdGenerator.cs
@@ -0,0 +1,37 @@
+using UnityEditor;
+using UnityEngine;
+
+public class SceneIdGenerator : MonoBehaviour
+{
+    private const string PATH = "Assets/ScreenNavigation/SceneID.cs";
+
+    // Menu item to manually regenerate the enum file
+    [MenuItem("Tools/Update Scene ID Enum")]
+    public static void GenerateSceneIdEnum()
+    {
+        // Build the enum content
+        string enumContent = "using System;\n\n" +
+                             "namespace ScreenNavigation\n" +
+                             "{\n" +
+                                 "\t[Serializable]\n" +
+                                 "\tpublic enum SceneID \n" +
+                                 "\t{\n";
+
+        var scenes = EditorBuildSettings.scenes;
+        for (int index = 0; index < scenes.Length; index++)
+        {
+            string scenePath = scenes[index].path;
+            string sceneName = System.IO.Path.GetFileNameWithoutExtension(scenePath)
+                                                .Replace(" ", "_")
+                                                .ToUpper();
+            enumContent += "\t\t" + sceneName + ",\n";
+        }
+        enumContent += "\t}\n}";
+
+        // Write the enum content to a file
+        System.IO.File.WriteAllText(PATH, enumContent);
+        AssetDatabase.Refresh(); // Refresh the AssetDatabase to load the new script
+
+        Debug.Log("SceneID.cs updated with current scenes!");
+    }
+}
--- Assets/ScreenNavigation/SceneID.cs
+++ Assets/ScreenNavigation/SceneID.cs
@@ -2,16 +2,17 @@ using System;
 
 namespace ScreenNavigation
 {
-    [Serializable]
-    public enum SceneID
-    {
-        DEBUG = 1,
-        HOME = 2,
-        SETTINGS = 3,
-        LOADOUT_SETUP = 4,
-        LOADING = 5,
-        GAME = 6,
-        INGAME_SETTINGS = 7,
-        GAME_OVER = 8,
-    }
+	[Serializable]
+	public enum SceneID 
+	{
+		MASTER,
+		DEBUG,
+		HOME,
+		SETTINGS,
+		LOADOUT_SETUP,
+		LOADING,
+		GAME,
+		INGAME_SETTINGS,
+		GAME_OVER,
+	}
 }
