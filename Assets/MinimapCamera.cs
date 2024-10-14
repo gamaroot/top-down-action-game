3f726a95697c5e672313f111227fc1b9585c8a92
JIMPARK80 <van0711@gmail.com>
2024-10-13 23:54:12 -0400

    Add Minimap
    Add minimap with UI

---
+++ Assets/MinimapCamera.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class MinimapCamera : MonoBehaviour
+{
+    public Transform player;
+
+    private void LateUpdate()
+    {
+        Vector3 newPosition = player.position;
+        newPosition.y = transform.position.y;
+        transform.position = newPosition;
+    }
+
+}
