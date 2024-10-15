b64d1a0ee63e4884a98f33c18a70adb40ae281a7
Artem Bobrov <art.bobro0v@gmail.com>
2024-10-14 23:16:44 -0400

    fix:damage overlaping is fixed, adapted for spike platform

--- Assets/Project/Modules/Game/Scripts/Spawn/TrapDamageController.cs
+++ Assets/Project/Modules/Game/Scripts/Spawn/TrapDamageController.cs
@@ -4,12 +4,38 @@ namespace Game
 {
     public class TrapDamageController : MonoBehaviour
     {
-        [SerializeField] private float damageAmount = 10f; 
+        [SerializeField] private float damageAmount = 1f; 
+        [SerializeField] private float damageInterval = 1f;  
+        private float lastDamageTime; 
+        private bool hasTriggered = false;
+
         private void OnTriggerEnter(Collider other)
+        {
+            if (other.gameObject.TryGetComponent(out PlayerHealthController playerHealth) && !hasTriggered)
+            {
+                playerHealth.ApplyDamage(damageAmount);
+                lastDamageTime = Time.time;
+                hasTriggered = true;  
+            }
+        }
+
+        private void OnTriggerStay(Collider other)
         {
             if (other.gameObject.TryGetComponent(out PlayerHealthController playerHealth))
             {
-                playerHealth.ApplyDamage(damageAmount);
+                if (Time.time >= lastDamageTime + damageInterval)
+                {
+                    playerHealth.ApplyDamage(damageAmount);
+                    lastDamageTime = Time.time;
+                }
+            }
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            if (other.gameObject.TryGetComponent(out PlayerHealthController playerHealth))
+            {
+                hasTriggered = false;
             }
         }
     }
